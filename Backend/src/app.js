c08ca4abcfe22218a33bc056533718e8fcc2691e
DoFlow-spec <majdeltacrypto@gmail.com>
2025-08-19 08:16:46 +0530

    dist

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: 'https://imagebox-six.vercel.app/',
     credentials: true,
   })
 );
